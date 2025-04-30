0e8e8bbe2d48f5af9898cf20c37746a552f9e1f2
jethrocsau <jethrocsa@gmail.com>
2025-04-30 21:59:09 +0800

    debug

--- utils/classifier.py
+++ utils/classifier.py
@@ -1,11 +1,13 @@
-import random
 import os
+import random
+
 import numpy as np
 import scipy as sp
-from models.load_model import load
+import torch
+
 from models.classifer_softmax import SoftMaxClassifier
 from models.ig import RNNHallucinationClassifier
-import torch
+from models.load_model import load
 
 # get the current directory
 utils_path = os.path.dirname(os.path.abspath(__file__))
@@ -29,7 +31,7 @@ def softmax_predict(model, results):
     # load results_activations
     with torch.no_grad():
         first_logits = np.stack([sp.special.softmax(i[j]) for i, j in zip(results['logits'], results['start_pos'])])
-        result = model(first_logits)    
+        result = model(first_logits)
     return result
 
 def RNN_predict(model, results):
@@ -39,7 +41,7 @@ def RNN_predict(model, results):
         x = results['attributes_first']
         preds = model(torch.tensor(x).view(1, -1, 1).to(torch.float).to(device))
         preds_softmax = torch.nn.functional.softmax(preds, dim=0)
-        result = preds_softmax[1].detach().cpu().numpy()
+        result = preds_softmax[0].detach().cpu().numpy()
     return result
 
 def attention_MLP_predict(model, results):
